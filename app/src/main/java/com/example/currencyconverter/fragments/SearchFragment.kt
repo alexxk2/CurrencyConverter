3ce4468817f303b4ee86867f339fa215a6a7dbb5
alexxk2 <alexx.koda@gmail.com>
2023-04-12 20:20:17 +0300

    29.03.23
    -сделал экран поиска и XML для ресайкл
    
    Задачи:
    
    -сделать recyclerView
    
    -сделать еще один экран для выбора валюты с recyclerView(добавить наверх плашку с текущим флагом/валютой, чтобы она развертывалась красиво обратно во флаг, посмотреть видео у лакнера про это "переход с картинкой") на этот же экран добавить передачу парамерта выбранной валюты. Этот экран должен работать для обеих плашек.Т.е. должна быть и прямая передача параметра и обратная. Также реализоваь sharedPref для хранения последних выбранных валют
    
    -добавить на главный экран информацию об актуальности последней конвертации(также её сохранять в sharedPref)
    
    -добавить крестики в edit texts
    -change stroke and hint colors
    -сделать темную тему со свитчером и sharedPref
    -сделать выбор языка (русский и английский) с sharedPref
    -сделать для всех вьюх стили, объединить где надо и сделать ночные стили
    
    -сделать логику, когда вводишь с одной стороны переводит на другую, если введено и там и там то сделать snackbar "оставьте одно поле пустым"

---
+++ app/src/main/java/com/example/currencyconverter/fragments/SearchFragment.kt
@@ -0,0 +1,47 @@
+package com.example.currencyconverter.fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.currencyconverter.databinding.FragmentSearchBinding
+import com.example.currencyconverter.viewmodels.SearchViewModel
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
