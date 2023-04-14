edf9f7d4452231215aecc3710f7e78c442cb3ecc
alexxk2 <alexx.koda@gmail.com>
2023-04-14 18:47:32 +0300

    29.03.23
    -сделал выбор и отображение флагов валют и прикрутил sharedPrefs к ним
    
    Задачи:
    
    -сделать еще один экран для выбора валюты с recyclerView(добавить наверх плашку с текущим флагом/валютой, чтобы она развертывалась красиво обратно во флаг, посмотреть видео у лакнера про это "переход с картинкой") на этот же экран добавить передачу парамерта выбранной валюты. Этот экран должен работать для обеих плашек.Т.е. должна быть и прямая передача параметра и обратная. Также реализоваь sharedPref для хранения последних выбранных валют
    
    -добавить на главный экран информацию об актуальности последней конвертации(также её сохранять в sharedPref)
    
    -добавить крестики в edit texts
    -change stroke and hint colors
    -сделать темную тему со свитчером и sharedPref
    -сделать выбор языка (русский и английский) с sharedPref
    -сделать для всех вьюх стили, объединить где надо и сделать ночные стили
    
    -сделать логику, когда вводишь с одной стороны переводит на другую, если введено и там и там то сделать snackbar "оставьте одно поле пустым"

--- app/src/main/java/com/example/currencyconverter/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/currencyconverter/fragments/SearchFragment.kt
@@ -1,6 +1,5 @@
 package com.example.currencyconverter.fragments
 
-
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,12 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.currencyconverter.adapters.CurrencyAdapter
 import com.example.currencyconverter.databinding.FragmentSearchBinding
+import com.example.currencyconverter.models.CurrencyInfo
 import com.example.currencyconverter.models.SearchService
 import com.example.currencyconverter.viewmodels.SearchViewModel
+import com.google.gson.Gson
 
 class SearchFragment : Fragment() {
 
@@ -23,12 +25,13 @@ class SearchFragment : Fragment() {
     private val viewModel: SearchViewModel by viewModels()
     private val searchService = SearchService()
     private lateinit var adapter: CurrencyAdapter
+    private var isLeftClicked = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         arguments?.let {
-
+            isLeftClicked = it.getBoolean(IS_LEFT)
         }
     }
 
@@ -45,9 +48,8 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
+
         setRecyclerView()
-
-
 
         binding.searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -79,17 +81,47 @@ class SearchFragment : Fragment() {
         else View.VISIBLE
     }
 
-
     private fun setRecyclerView() {
-        adapter = CurrencyAdapter(searchService.listOfCurrencies, requireContext())
+        adapter = CurrencyAdapter(searchService.listOfCurrencies, requireContext(), object: CurrencyAdapter.CurrencyActionListener{
+            override fun onClickCurrency(currencyInfo: CurrencyInfo) {
+                val action = SearchFragmentDirections.actionSearchFragmentToConverterFragment()
+                navigate(action)
+
+                if (isLeftClicked) putSharedPrefs(LEFT_CURRENCY, currencyInfo)
+                else putSharedPrefs(RIGHT_CURRENCY, currencyInfo)
+
+            }
+
+        })
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.setHasFixedSize(true)
     }
 
+    private fun putSharedPrefs(side: String, currencyInfo: CurrencyInfo){
+        val sharedPrefs = activity?.getSharedPreferences(SHARED_PREFS,0)
+        val jSonCurrency = Gson().toJson(currencyInfo, CurrencyInfo::class.java)
+        sharedPrefs?.let {
+            it.edit()
+                .putString(side, jSonCurrency)
+                .apply()
+        }
+
+    }
+
+    private fun navigate(action: NavDirections){
+        findNavController().navigate(action)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object{
+        const val IS_LEFT = "isLeftClicked"
+        const val LEFT_CURRENCY = "left_currency"
+        const val RIGHT_CURRENCY = "right_currency"
     }
 
 }
